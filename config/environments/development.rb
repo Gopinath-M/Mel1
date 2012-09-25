99c59b22fe9a93bc6a845d797f37cb6486959df0
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-09-25 15:13:59 +0530

    updated layout, forget password, config for development

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -25,5 +25,18 @@ Melaka::Application.configure do
 
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
+
+  config.action_mailer.default_url_options = { :host => 'localhost:3000'}
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => 'gmail.com',
+    :user_name            => 'edvinjoy',
+    :password             => 'testtest',
+    :authentication       => 'plain',
+    :enable_starttls_auto => true  }
+  
 end
 
--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,8 +1,21 @@
-// Place your application-specific JavaScript functions and classes here
-// This file is automatically included by javascript_include_tag :defaults
 $().ready(function(){
     $(function() {
         $("input").watermark({
+            placeholder: function() {
+                return $(".formRow label[for=" + this.id + "]").hide().text();
+            }
+        });
+        $("input[type='text']").watermark({
+            placeholder: function() {
+                return $(".formRow label[for=" + this.id + "]").hide().text();
+            }
+        });
+        $("input[type='password']").watermark({
+            placeholder: function() {
+                return $(".formRow label[for=" + this.id + "]").hide().text();
+            }
+        });
+        $("textarea").watermark({
             placeholder: function() {
                 return $(".formRow label[for=" + this.id + "]").hide().text();
             }
