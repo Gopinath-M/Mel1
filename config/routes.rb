13fa4ff4966fb36505690dedbdeba95f72154188
Manivannan <manisiva19@gmail.com>
2012-09-25 11:11:43 +0530

    added routes for dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Melaka::Application.routes.draw do
+  resources :dashboard 
   devise_for :users
 
   # The priority is based upon order of creation:
@@ -50,7 +51,7 @@ Melaka::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+      root :to => "dashboard#index"
 
   # See how all your routes lay out with "rake routes"
 
