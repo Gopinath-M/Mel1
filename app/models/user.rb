9bee935d9c1851e7a1b63d8af8b577e0e0c68eea
Manivannan <manisiva19@gmail.com>
2012-09-27 16:01:38 +0530

    commented the database authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,9 @@ class User < ActiveRecord::Base
 
   attr_accessible :email, :password, :password_confirmation, :remember_me,:ic_number
 
-  def self.find_for_database_authentication(conditions={})
-   self.where("ic_number = ?", conditions[:ic_number]).limit(1).first
-  end
+  #I dont think so this method is needed. Becaz, if we are going to do authentication based on two fields we can use this method. Since we are doing authentication based on IC number only, it is not needed. I checked the sign in and sign up. It's working good. Nirmala, IF u think My suggestion is ok, Please remove it. while you are seeing this msg.
+  #def self.find_for_database_authentication(conditions={})
+   # self.where("ic_number = ?", conditions[:ic_number]).limit(1).first
+  #end
   
 end
