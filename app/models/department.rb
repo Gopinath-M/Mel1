d577f1d70cc69fe439e3e5d201909b1a9c5d71f5
Manivannan <manisiva19@gmail.com>
2012-10-19 16:10:16 +0530

    added associations

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,4 +1,6 @@
 class Department < ActiveRecord::Base
-  has_many :users
+  has_many :role_memberships
+  has_many :roles
+  has_many :users, :through => :role_memberships, :uniq => true
   validates :name, :presence => true
 end
