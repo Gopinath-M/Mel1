1ae1b80e0dacee72fb0ad31a818fe41ed6a257cc
sathish <sathish.thandavan@gmail.com>
2012-10-19 11:31:19 +0530

    dashboard display

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,12 @@ class DashboardController < ApplicationController
   before_filter :authenticate_user!
 #  layout 'main_content'
   def index
-    @users = User.all
+    if current_user.role_id == 1
+     @users=User.all(:conditions=>["role_id = 2"])
+    elsif current_user.role_id == 2
+       @users=User.all(:conditions=>["role_id = 3"])
+    else
+@users=User.all(:conditions=>["role_id = 3"])
   end
-
+  end
 end
