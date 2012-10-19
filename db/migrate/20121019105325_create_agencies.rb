a389fcaf14e19ebfd06b148d4bcf9205ddbd5300
Manivannan <manisiva19@gmail.com>
2012-10-19 16:24:25 +0530

    added agency table

---
+++ db/migrate/20121019105325_create_agencies.rb
@@ -0,0 +1,13 @@
+class CreateAgencies < ActiveRecord::Migration
+  def self.up
+    create_table :agencies do |t|
+      t.string :name
+      t.string :status
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :agencies
+  end
+end
