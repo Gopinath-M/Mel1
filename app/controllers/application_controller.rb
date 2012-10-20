89cbbdbec1e1b70946a2cf9bf751999426c689d1
Manivannan <manisiva19@gmail.com>
2012-10-20 21:17:30 +0530

    added method for current department

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,24 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  #this will work if the user belongs to one department. We need to do for multiple departments #manivannan
+  def current_department  
+    @current_department ||= current_user.departments.first
+  end
+
+  # this method is for restricting user from login before super admin approves him. Since it has to be needed some tweaks, i am commenting this for timebeing #manivannan
+  #def validate_user_role_membership  
+     # if (current_user.present? && current_department.present?)
+        #rm = current_user.role_memberships.where(:department_id => current_department.id).first
+        #if not rm.nil?      
+         # if rm.status == STATUS_INACTIVE        
+            #sign_out(current_user)
+            #render :template => "errors/blocked.html.erb" 
+          #end
+        #end
+      #end  
+  #end    
+
   def is_admin
     if !((current_user.roles.first.name=="Super Admin" || current_user.roles.first.name=="Department Admin") && current_user.role_memberships.first.status=="A")
       redirect_to dashboard_index_path
