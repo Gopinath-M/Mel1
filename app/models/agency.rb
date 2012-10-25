625de0a2d81540730bc8d42d3261cda58d205258
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-10-25 19:09:14 +0530

    updated uniqness in models

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -3,7 +3,7 @@ class Agency < ActiveRecord::Base
   #validations comes here
   validates :name, :presence => true
   validates :name, :uniqueness => true
-  validate_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
+  validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
   #named Scopes comes here
   scope :active, where(:is_active => true, :deleted => false)
    
--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,7 +1,7 @@
 class Unit < ActiveRecord::Base
   #validations here
   validates :name, :presence => true
-  validate_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
+  validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
   #named Scopes comes here
   scope :active, where(:is_active => true, :deleted => false)
   validates :department_id ,:presence=>true
