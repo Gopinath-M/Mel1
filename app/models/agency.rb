3351b0775cc5a5be2fc0a4140c1ab0ccfca68317
Manivannan <manisiva19@gmail.com>
2012-10-25 17:51:29 +0530

    added uniqueness for name

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,6 +1,7 @@
 class Agency < ActiveRecord::Base
   #validations comes here
   validates :name, :presence => true
+  validates :name, :uniqueness => true
 
   #named Scopes comes here
   scope :active, where(:is_active => true, :deleted => false)
