3b63b82b48d3492eefc86238d3589ce0d48ff962
sathish <sathish.thandavan@gmail.com>
2012-10-29 19:18:17 +0530

    default options set

--- db/migrate/20120919111125_devise_create_users.rb
+++ db/migrate/20120919111125_devise_create_users.rb
@@ -41,8 +41,8 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.boolean :deleted, :default => false
       t.column :activation_code, :string, :limit => 40
       t.column :activated_at, :datetime
-      t.boolean :widget_one, :default => true
-      t.boolean :widget_two, :default => true
+      t.boolean :widget_one, :default => 1
+      t.boolean :widget_two, :default => 1
       t.boolean :profile_status, :default => 0
       # t.encryptable
       # t.confirmable
