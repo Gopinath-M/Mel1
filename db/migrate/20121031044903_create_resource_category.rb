e63baa3c1745bcb20c9d361bf7e6ac85ecde381a
sathish <sathish.thandavan@gmail.com>
2012-11-02 17:53:51 +0530

    resource category migration

---
+++ db/migrate/20121031044903_create_resource_category.rb
@@ -0,0 +1,14 @@
+class CreateResourceCategory < ActiveRecord::Migration
+  def self.up
+    create_table :resource_categories do |t|
+      t.string :name
+      t.boolean :is_active
+      t.boolean :deleted, :default => false
+      t.timestamps
+    end
+  end
+
+  def self.down
+	   drop_table :resource_categories
+  end
+end
