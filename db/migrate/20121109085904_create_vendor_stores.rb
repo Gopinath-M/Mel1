89c363e5a18f37e703c492bc5fd9571d7627f870
sathish <sathish.thandavan@gmail.com>
2012-11-17 19:33:12 +0530

    vendor store default true

--- db/migrate/20121109085904_create_vendor_stores.rb
+++ db/migrate/20121109085904_create_vendor_stores.rb
@@ -2,9 +2,9 @@ class CreateVendorStores < ActiveRecord::Migration
   def self.up
     create_table :vendor_stores do |t|
       t.references :vendor
-      t.integer :resources
+      t.references :resources
       t.integer :quantity
-      t.string :serial_no
+      t.text :serial_no
       t.integer :uom
       t.boolean :is_active
       t.boolean :deleted, :default => false
