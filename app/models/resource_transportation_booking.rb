a420bb603cdceac918612a64e23696b28134bbf0
mathew <senthilkumar.s@openwavecomp.in>
2012-11-20 10:47:52 +0530

    resource transport booking changes

--- app/models/resource_transportation_booking.rb
+++ app/models/resource_transportation_booking.rb
@@ -1,4 +1,5 @@
 class ResourceTransportationBooking < ActiveRecord::Base
-  belongs_to :resource  
+  belongs_to :resource    
   validates :resource_id,:purpose,:number_of_passengers,:pick_up_place,:requested_from_date,:requested_to_date,:location, :presence => true
+  mount_uploader :transport_avatar, TransportAvatarUploader
 end
