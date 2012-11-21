7aa6a79c115f0acad4cc74e03eb75168cef1054e
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-11-21 19:34:12 +0530

    updated for user navigation restriction

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -252,10 +252,10 @@ class UsersController < ApplicationController
   def update_default_department
     department=Department.find(params[:default][:department_id]) if params[:default][:department_id]
     if department && department!=nil
-     p role=current_user.role_memberships.where(:default_dept => true)
-     p role.first.update_attribute(:default_dept, false) if role && !role.empty? && role.first
-    p  new_role=current_user.role_memberships.where(:default_dept => false,:department_id=>department.id)
-    p  new_role.first.update_attribute(:default_dept, true)  if new_role && !new_role.empty? && new_role.first
+      role=current_user.role_memberships.where(:default_dept => true)
+      role.first.update_attribute(:default_dept, false) if role && !role.empty? && role.first
+      new_role=current_user.role_memberships.where(:default_dept => false,:department_id=>department.id)
+      new_role.first.update_attribute(:default_dept, true)  if new_role && !new_role.empty? && new_role.first
     end
     redirect_to(users_path, :notice => "Your Account Settings Updated successfully")
   end
