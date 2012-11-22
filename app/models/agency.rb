01f63f2c29e922600e8f21620669e0b24309ac9d
sathish <sathish.thandavan@gmail.com>
2012-11-22 11:47:46 +0530

    conflict fixed

--- app/models/agency.rb
+++ app/models/agency.rb
@@ -1,11 +1,14 @@
 class Agency < ActiveRecord::Base
+
   has_many :departments
   has_many :resource_managers
-  #validations comes here
-  validates :name, :presence => true
-  validates :name, :uniqueness => true
-  validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
   has_one :agency_store
+  
+  #validations comes here
+  validates :name,:address,:telephone_number, :presence => true
+  validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
+  validates_format_of :telephone_number, :with=>/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+  
   #named Scopes comes here
   scope :active, where(:is_active => true, :deleted => false)
    
