f438d33fb740971623ad963bb79913c2110025fa
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-11-22 20:19:28 +0530

    updated the validations for category and sub category

--- app/models/sub_category.rb
+++ app/models/sub_category.rb
@@ -1,6 +1,6 @@
 class SubCategory < ActiveRecord::Base
   validates :name,:category_id, :presence => true
-  validates :name, :uniqueness => true
+  validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
 
   has_many :resources
   belongs_to :category
