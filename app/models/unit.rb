ff81df380c888630af814518d3a8cb1043d2a318
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-11-22 14:46:42 +0530

    updated seed and model for validation

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -4,9 +4,9 @@ class Unit < ActiveRecord::Base
   has_many :users, :through => :role_memberships
 
   attr_accessor :agency
-  validates :agency, :name,:telephone_number,:address, :presence => true
+  validates :agency, :name,:address,:telephone_number, :presence => true
   validates_uniqueness_of :name, :case_sensitive=>false, :if=>Proc.new {|u| !u.name.blank?}
-  validates_format_of :telephone_number, :with=>/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/
+  validates_format_of :telephone_number, :with=>/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, :if=>Proc.new{|u| !u.telephone_number.blank?}
   validates_format_of :fax_number, :with=>/^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/, :if=>Proc.new{|u| !u.fax_number.blank?}
 
   #Named scopes comes here
