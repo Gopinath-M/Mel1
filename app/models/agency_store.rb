8e09d31f4d8f7814f8423ab95d1df6c0d84e6640
sathish <sathish.thandavan@gmail.com>
2012-11-22 17:40:26 +0530

    validation removed

--- app/models/agency_store.rb
+++ app/models/agency_store.rb
@@ -2,5 +2,5 @@ class AgencyStore < ActiveRecord::Base
 #  has_many :resource_bookings, :as=>:bookable
   belongs_to :agency
   scope :active, where(:is_active => true, :deleted => false)
-  validates :quantity, :presence=>true
+#  validates :quantity, :presence=>true
 end
