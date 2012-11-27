7ce21fc27067a9e84d9d688ea611568ab7fa6b05
sathish <sathish.thandavan@gmail.com>
2012-11-27 10:52:01 +0530

    default active is true

--- db/migrate/20121105063029_create_vendors.rb
+++ db/migrate/20121105063029_create_vendors.rb
@@ -3,11 +3,10 @@ class CreateVendors < ActiveRecord::Migration
     create_table :vendors do |t|
       t.string :name
       t.string :remarks
-      t.boolean :is_active
+      t.boolean :is_active, :default => true
       t.text :address
       t.text :email
       t.string :contact_no
-#      t.string :vendor_type
       t.timestamps
     end
   end
