c7509df5689bbcc695d1ebe64b4899675568f504
sathish <sathish.thandavan@gmail.com>
2012-11-27 10:50:20 +0530

    default active is true

--- db/migrate/20120926102736_create_units.rb
+++ db/migrate/20120926102736_create_units.rb
@@ -7,7 +7,7 @@ class CreateUnits < ActiveRecord::Migration
       t.string :telephone_number
       t.string :fax_number
       t.integer :order_by
-      t.boolean :is_active
+      t.boolean :is_active, :default => true
       t.boolean :deleted, :default => false
       t.integer :created_by
       t.integer :updated_by
