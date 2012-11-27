6fc3a5bff04373787f4e2dc5d44ad55f5a6d9fed
sathish <sathish.thandavan@gmail.com>
2012-11-27 10:50:31 +0530

    default active is true

--- db/migrate/20120930093559_create_roles.rb
+++ db/migrate/20120930093559_create_roles.rb
@@ -2,7 +2,7 @@ class CreateRoles < ActiveRecord::Migration
   def self.up
     create_table :roles do |t|
       t.string :name
-      t.boolean :is_active
+      t.boolean :is_active, :default => true
       t.boolean :deleted, :default => false
       t.timestamps
     end
