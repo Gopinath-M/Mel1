b6704f241447e4abe4aff1fa9de5608601f40e89
sathish <sathish.thandavan@gmail.com>
2012-11-27 10:51:10 +0530

    default active is true

--- db/migrate/20121031044903_create_category.rb
+++ db/migrate/20121031044903_create_category.rb
@@ -2,7 +2,7 @@ class CreateCategory < ActiveRecord::Migration
   def self.up
     create_table :categories do |t|
       t.string :name
-      t.boolean :is_active
+      t.boolean :is_active, :default => true
       t.boolean :deleted, :default => false
       t.integer :created_by
       t.timestamps
