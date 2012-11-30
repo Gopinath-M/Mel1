76ed40792def7c5f53226dc78fc58194d28413f3
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-11-30 19:04:44 +0530

    Department name is titlized in seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,7 +40,7 @@ begin
       n=18
       agency_id=(1..n).to_a.sample
       record_split = record.split(",")
-      Department.create!(:agency_id => agency_id, :name=>record_split[0],:address=>record_split[1],:telephone_number=>record_split[2], :is_active=>true, :created_by=>1, :updated_by=>1)
+      Department.create!(:agency_id => agency_id, :name=>record_split[0].titleize,:address=>record_split[1],:telephone_number=>record_split[2], :is_active=>true, :created_by=>1, :updated_by=>1)
     rescue Exception =>e
       p "Exception ocurred due to #{e.to_s} at #{i}"
     end
