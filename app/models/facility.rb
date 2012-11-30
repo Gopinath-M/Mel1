f45de0f3710144df6ae35bd2df87f95304967058
sathish <sathish.thandavan@gmail.com>
2012-11-30 16:07:35 +0530

    validation by sasi

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,6 +1,6 @@
 class Facility < ActiveRecord::Base
   validates :resource_type, :presence => true
-  validates :room_id, :presence => true
+#  validates :sub_category_id, :presence => true
   validates :name, :presence => true
   validates :total_qty, :presence => true
 end
