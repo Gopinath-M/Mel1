c6de4adaaa1fac60290aa7162c7db74a49b88003
sathish <sathish.thandavan@gmail.com>
2012-11-30 16:27:43 +0530

    facility

--- db/migrate/20121122200738_create_resource_room_bookings.rb
+++ db/migrate/20121122200738_create_resource_room_bookings.rb
@@ -1,12 +1,18 @@
 class CreateResourceRoomBookings < ActiveRecord::Migration
   def self.up
     create_table :resource_room_bookings do |t|
-      t.integer :room_type_id
+      t.integer :sub_category_id
+      t.integer :resource_id
+      t.integer :agency_store_id
       t.integer :user_id
+      t.integer :department_id
       t.string :requested_from_date
       t.string :requested_to_date
+      t.boolean :user_returned_status
       t.integer :room_capacity
       t.string :purpose
+      t.text :remarks
+      t.string :room_attachment
       t.string :status
       t.timestamps
     end
