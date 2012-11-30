f11444ef77adba051f7048e80004f4699191312d
sathish <sathish.thandavan@gmail.com>
2012-11-30 14:11:34 +0530

    validation added

--- app/models/agency_store.rb
+++ app/models/agency_store.rb
@@ -2,5 +2,7 @@ class AgencyStore < ActiveRecord::Base
 #  has_many :resource_bookings, :as=>:bookable
   belongs_to :agency  
   has_many :resource_transportation_bookings
+
+  validates :agency_id, :sub_category_id, :resource_id, :presence=>true
   scope :active, where(:is_active => true, :deleted => false)
 end
