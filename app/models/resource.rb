dfdba76a5b14d4f5f9d740bb154eaf50e727a7e7
sathish <sathish.thandavan@gmail.com>
2012-11-30 16:08:14 +0530

    validation

--- app/models/resource.rb
+++ app/models/resource.rb
@@ -3,11 +3,9 @@ class Resource < ActiveRecord::Base
   belongs_to :department
   belongs_to :category
   belongs_to :sub_category
-  has_many :vendors, :through => :resource_vendors
-  has_many :resource_managers
   has_many :resource_boookings
   has_many :resource_transport_bookings
   
-  validates :category_id,:sub_category_id,:resource_type, :presence=>true
+  validates :sub_category_id, :presence=>true
   scope :active, where(:deleted => false)
 end
