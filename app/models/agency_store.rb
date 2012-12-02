981a16a5323677172ecbacea2a823b9f691488e4
sathish <sathish.thandavan@gmail.com>
2012-12-02 20:47:09 +0530

    model validation removed

--- app/models/agency_store.rb
+++ app/models/agency_store.rb
@@ -3,6 +3,6 @@ class AgencyStore < ActiveRecord::Base
   belongs_to :agency  
   has_many :resource_transportation_bookings
 
-  validates :agency_id, :sub_category_id, :resource_id, :presence=>true
+#  validates :agency_id, :sub_category_id, :resource_id, :presence=>true
   scope :active, where(:is_active => true, :deleted => false)
 end
