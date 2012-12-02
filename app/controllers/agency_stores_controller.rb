a53ea541f642c7ba69a778d8ef5d44f8d03a06bc
sathish <sathish.thandavan@gmail.com>
2012-12-02 20:44:07 +0530

    conflict fixed for tranport status

--- app/controllers/agency_stores_controller.rb
+++ app/controllers/agency_stores_controller.rb
@@ -2,12 +2,22 @@ class AgencyStoresController < ApplicationController
   before_filter :authenticate_user!, :except=>[:activate]
   before_filter :is_admin
   def index
+    @resource_id = params[:resource_id].to_i # while selecting Please Select returns string params
+    if @resource_id == 0
     if params[:id].blank? || params[:id].nil?
       if current_user.is_super_admin?
         @stores=AgencyStore.order.page(params[:page]).per(15)
       else
-        @stores=AgencyStore.find_all_by_department_id(current_department.id)
+        @stores=AgencyStore.find_all_by_agency_id(@resource_id)
       end
+    end
+    else
+       if current_user.is_super_admin?
+        @stores=AgencyStore.find_all_by_agency_id(@resource_id)
+      end
+    end
+     if request.xhr?
+      render :layout=>false
     end
   end
 
@@ -23,15 +33,19 @@ class AgencyStoresController < ApplicationController
     @store = AgencyStore.create(params[:agency_store])
     @store.resource_type = params[:resource_type]
     if params[:room_agency]
-      @store.sub_category_id = params[:room_agency][:sub_category_id]
-      @store.resource_id = params[:room_agency][:resource_id]
+    @store.agency_id = params[:room][:agency_id]
+    @store.sub_category_id = params[:room_agency][:sub_category_id]
+    @store.resource_id = params[:room_agency][:resource_id]
     elsif params[:transport_agency]
-      @store.sub_category_id = params[:transport_agency][:sub_category_id]
-      @store.resource_id = params[:transport_agency][:resource_id]
-      SubCategory.find(@store.sub_category_id).update_attribute(:is_available,true)
+    @store.agency_id = params[:transport][:agency_id]
+    @store.sub_category_id = params[:transport_agency][:sub_category_id]
+    @store.resource_id = params[:transport_agency][:resource_id]
+    SubCategory.find(@store.sub_category_id).update_attribute(:is_available,true)
     elsif params[:other_agency]
-      @store.sub_category_id = params[:other_agency][:sub_category_id]
-      @store.resource_id = params[:other_agency][:resource_id]
+    @store.agency_id = params[:other][:agency_id]
+    @store.category_id = params[:other_category][:id]
+    @store.sub_category_id = params[:other_agency][:sub_category_id]
+    @store.resource_id = params[:other_agency][:resource_id]
     end
     #    store.categories_id = params[:categories_department][:id]
     #    store.sub_categories_id = params[:sub_categories][:id]
@@ -99,5 +113,20 @@ class AgencyStoresController < ApplicationController
   def get_vehicles
     vehicles = Vehicle.find_all_by_vehicle_type_id(params[:vehicle_id])
     render :json=>[ vehicles] if  vehicles
+  end
+  def get_resources
+     agency_stores = AgencyStore.find_by_id(params[:resource_id])
+     if agency_stores != nil
+     sub_category = SubCategory.find_all_by_id(agency_stores.sub_category_id)
+     render :json=>[ sub_category] if  sub_category
+     else
+       sub_category = 0
+       render :json=>[ sub_category] if  sub_category
+     end
+    
+  end
+  def get_other_sub_categories
+    subcategories = SubCategory.find_all_by_category_id(params[:agency_id])
+    render :json=>[ subcategories] if  subcategories
   end
 end
