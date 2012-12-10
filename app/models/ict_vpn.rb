f243e887ec4e4c7dff3d1d66b7dd02286c96639f
sathish <sathish.thandavan@gmail.com>
2012-12-10 18:30:39 +0530

    date validation for ict vpn

--- app/models/ict_vpn.rb
+++ app/models/ict_vpn.rb
@@ -11,10 +11,10 @@ class IctVpn < ActiveRecord::Base
   validate :validate_booking_time
 
   def validate_booking_time
-    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date!=nil && self.from_date < Time.now
-    errors.add(:base,"To Date Should be greater than current date and time") if self.to_date!=nil && self.to_date < Time.now
-    errors.add(:base,"To Date Should be greater than From date and time") if self.from_date!=nil && self.to_date!=nil && self.from_date > self.to_date
-    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date!=nil && self.from_date < Time.now
+    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
+    errors.add(:base,"To Date Should be greater than current date and time") if self.to_date.to_datetime!=nil && self.to_date.to_datetime < Time.now
+    errors.add(:base,"To Date Should be greater than From date and time") if self.from_date.to_datetime!=nil && self.to_date.to_datetime!=nil && self.from_date.to_datetime > self.to_date.to_datetime
+    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
     end
 
     mount_uploader :vpn_attachment, VpnAttachmentUploader
