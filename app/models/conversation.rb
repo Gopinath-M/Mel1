ab9623540b3c00c99c3da983a63f41351d8ada6c
mathew <senthilkumar.s@openwavecomp.in>
2012-12-11 16:48:53 +0530

    Conversation Postgres Fixes - Sheik

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -2,7 +2,7 @@ class Conversation < ActiveRecord::Base
   belongs_to :user, :counter_cache => true
 
   scope:convos, lambda { |*args|
-    {:conditions=>["(from_userid =? and to_userid=?) or ((from_userid =? and to_userid=?))", args[0],args[1],args[1],args[0]],:group=>"created_at asc"}
+    {:conditions=>["(from_userid =? and to_userid=?) or ((from_userid =? and to_userid=?))", args[0],args[1],args[1],args[0]]}
   }
   
 end
