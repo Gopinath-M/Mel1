18025d92e4e94a20a08fae9ea516e670d39dd1c5
mathew <senthilkumar.s@openwavecomp.in>
2012-12-12 16:09:38 +0530

    Ict Firewall Issues

--- app/controllers/ict_firewalls_controller.rb
+++ app/controllers/ict_firewalls_controller.rb
@@ -24,6 +24,7 @@ class IctFirewallsController < ApplicationController
     @ict_firewall.source_ip = params[:s1]+'.'+params[:s2]+'.'+params[:s3]+'.'+params[:s4]
     @ict_firewall.destination_ip = params[:d1]+'.'+params[:d2]+'.'+params[:d3]+'.'+params[:d4]    
     @ict_firewall.status = 'New'
+    @ict_firewall.department_id = params[:department_id]
     @ict_firewall.requisition_type_id = '2'
     @ict_firewall.requested_from_date = params[:ict_firewall][:requested_from_date].to_datetime
     @ict_firewall.requested_to_date = params[:ict_firewall][:requested_to_date].to_datetime
@@ -47,10 +48,12 @@ class IctFirewallsController < ApplicationController
   def update
     @ict_firewall = IctFirewall.find(params[:id])
   
-    if params[:ict_firewall][:status].to_s == 'Approve'
-      t = true
-    else
-      t = false
+    if @ict_firewall.status == "New"
+      if params[:ict_firewall][:status].to_s == 'Approve'
+        t = true
+      else
+        t = false
+      end
     end
   
     if @ict_firewall.status == 'New'
