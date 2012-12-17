d62097a9f5fefc192fd4dd00dfba0bf05a76a153
sathish <sathish.thandavan@gmail.com>
2012-12-17 17:00:49 +0530

    complaints module for Sheik

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -36,42 +36,42 @@ class UserMailer < ActionMailer::Base
     mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been forwarded request for ICT - #{@requisition_ict_vpn.name} Booking")
   end
 
- def ict_vpn(ict_email, ict_vpn, requisition_ict_vpn, system_access_ict_vpn, requisition_user)
+def send_mail_to_ict_vpn(ict_email, ict_vpn, requisition_ict_vpn, system_access_ict_vpn, requisition_user)
     @user = ict_email
     @ict_vpn = ict_vpn
     @requisition_ict_vpn = requisition_ict_vpn
     @system_access_ict_vpn = system_access_ict_vpn
     @requisition_user = requisition_user
-    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been forwarded request for ICT - #{@requisition_ict_vpn.name} Booking")
+    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been requested for ICT - #{@requisition_ict_vpn.name} Booking")
   end
 
-  def ict_system_access(ict_email, ict_system_access, requisition_ict_system_access, system_access_ict_system_access, requisition_user)
+  def send_mail_to_ict_system_access(ict_email, ict_system_access, requisition_ict_system_access, system_access_ict_system_access, requisition_user)
     @user = ict_email
     @ict_system_access = ict_system_access
     @requisition_ict_system_access = requisition_ict_system_access
     @system_access_ict_system_access = system_access_ict_system_access
     @requisition_user = requisition_user
-    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been forwarded request for ICT - #{@requisition_ict_system_access.name} Booking")
+    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been requested for ICT - #{@requisition_ict_system_access.name} Booking")
   end
 
-  def complaint_building_asset(ict_email, complaint_building_asset, name, demage_name, item_name, requisition_user)
+  def send_mail_to_complaint_building_asset(ict_email, complaint_building_asset, category_name, type_name, item_name, requisition_user)
     @user = ict_email
     @complaint_building_asset = complaint_building_asset
-    @name = name
-    @demage_name = demage_name
+    @category_name = category_name
+    @type_name = type_name
     @item_name = item_name
     @requisition_user = requisition_user
-    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been forwarded complained request for Building Asset - #{@name.name}")
+    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been complained request for Building Asset - #{@category_name.name}")
   end
 
-  def complaint_computer(ict_email, complaint_computer, name, system_access_name, system_model_name, requisition_user)
+  def send_mail_to_complaint_computer(ict_email, complaint_computer, name, system_access_name, system_model_name, requisition_user)
     @user = ict_email
     @complaint_computer = complaint_computer
     @name = name
     @system_access_name = system_access_name
     @system_model_name = system_model_name
     @requisition_user = requisition_user
-    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been forwarded complained request for Computers - #{@name.name}")
+    mail(:from=>@requisition_user.email,:to=>@user.email,:subject =>"#{@requisition_user.username}! has been complained request for Computers - #{@name.name}")
   end
 
   def send_mail_to_approver1(user,dept_admin,department)
