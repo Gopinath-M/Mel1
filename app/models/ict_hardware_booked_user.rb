20dbd22ba7c505b5a60a272986c2b8832a1eb02b
nirmala <nirmala@OWC-DT-O-046.(none)>
2012-12-18 19:10:03 +0530

    updated models

--- app/models/ict_hardware_booked_user.rb
+++ app/models/ict_hardware_booked_user.rb
@@ -1,8 +1,19 @@
 class IctHardwareBookedUser < ActiveRecord::Base
-  has_many :ict_hardware_booked_statuses
-    belongs_to :user, :class_name => "User", :foreign_key => "user_id"
-    belongs_to :officer, :class_name => "User", :foreign_key => "officer_id"
+#  has_many :ict_hardware_booked_statuses
+  belongs_to :ict_hardware_booking
+  belongs_to :user, :class_name => "User", :foreign_key => "user_id"
+  belongs_to :officer, :class_name => "User", :foreign_key => "officer_id"
   belongs_to :approver, :class_name => "User", :foreign_key => "approver_id"
 
-  accepts_nested_attributes_for :ict_hardware_booked_statuses
+#  accepts_nested_attributes_for :ict_hardware_booked_statuses
+
+  validates :user_id, :presence=>true
+  validate :booking
+  
+  def booking
+    errors.add(:base,"Select if hardware is replacement or new") if self.is_replacement.nil?
+    errors.add(:base,"Select atleast one required hardware for requistion") if self.pc.blank? && self.printer.blank? && self.scanner.blank?
+  end
+
+
 end
