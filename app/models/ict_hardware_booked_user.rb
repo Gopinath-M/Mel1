1c0111e2a5e2865b2b5182494e56c6562e68d7e3
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-01-03 11:15:38 +0530

    updated internationalization for ict hardware booked status

--- app/models/ict_hardware_booked_user.rb
+++ app/models/ict_hardware_booked_user.rb
@@ -1,18 +1,22 @@
 class IctHardwareBookedUser < ActiveRecord::Base
-#  has_many :ict_hardware_booked_statuses
+  #  has_many :ict_hardware_booked_statuses
   belongs_to :ict_hardware_booking
   belongs_to :user, :class_name => "User", :foreign_key => "user_id"
   belongs_to :officer, :class_name => "User", :foreign_key => "forward_to"
 
-#  accepts_nested_attributes_for :ict_hardware_booked_statuses
+  #  accepts_nested_attributes_for :ict_hardware_booked_statuses
 
   validates :user_id, :presence=>true
   validate :booking
   
   def booking
-    errors.add(:base,"Select if hardware is replacement or new") if self.is_replacement.nil?
-    errors.add(:base,"Select atleast one required hardware for requistion") if self.pc.blank? && self.printer.blank? && self.scanner.blank?
+    errors.add(:base,(I18n.translate!('errors_date.hardware_replace'))) if self.is_replacement.nil?
+    errors.add(:base,(I18n.translate!('errors_date.select_one_hardware'))) if self.pc.blank? && self.printer.blank? && self.scanner.blank?
   end
 
-
+#  after_save :update_status_and_remarks
+#  def update_status_and_remarks
+#    status = ApplicationStatus.create(:module_name => MODULE_NAME_REQUISITON, :module_type => MODULE_TYPE_REQ_HARDWARE, :module_resource_id => self.id, :status => self.status, :remarks => self.remarks, :notify_recipient => 1)
+#    return status
+#  end
 end
