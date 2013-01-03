83fc765f86cb6b65167f7ffdbe9317d55821e027
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-01-03 11:17:46 +0530

    updated internationalization for models

--- app/models/ict_vpn.rb
+++ app/models/ict_vpn.rb
@@ -11,10 +11,10 @@ class IctVpn < ActiveRecord::Base
   validate :validate_booking_time
 
   def validate_booking_time
-    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
-    errors.add(:base,"To Date Should be greater than current date and time") if self.to_date.to_datetime!=nil && self.to_date.to_datetime < Time.now
-    errors.add(:base,"To Date Should be greater than From date and time") if self.from_date.to_datetime!=nil && self.to_date.to_datetime!=nil && self.from_date.to_datetime > self.to_date.to_datetime
-    errors.add(:base,"From Date Should be greater than current date and time") if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
+    errors.add(:base,(I18n.translate!('errors_date.date_greater'))) if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
+    errors.add(:base,(I18n.translate!('errors_date.to_date_greater'))) if self.to_date.to_datetime!=nil && self.to_date.to_datetime < Time.now
+    errors.add(:base,(I18n.translate!('errors_date.from_date_greater'))) if self.from_date.to_datetime!=nil && self.to_date.to_datetime!=nil && self.from_date.to_datetime > self.to_date.to_datetime
+    errors.add(:base,(I18n.translate!('errors_date.date_greater'))) if self.from_date.to_datetime!=nil && self.from_date.to_datetime < Time.now
     end
 
     mount_uploader :vpn_attachment, VpnAttachmentUploader
