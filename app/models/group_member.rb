fafb3770af4e23f5cc6c6d22facfddbcef2c46ad
Mathew <mathew@OWC-DT-O-010.(none)>
2013-01-08 15:44:37 +0530

    Group Issues

--- app/models/group_member.rb
+++ app/models/group_member.rb
@@ -1,5 +1,6 @@
 class GroupMember < ActiveRecord::Base
   belongs_to :group
   #belongs_to :user
+  validates :group_id, :user_id, :presence => true
   validates :user_id, :uniqueness => {:scope => :group_id}
 end
