b7317c4169dc48b301eafc0844fb255daa8b6fc1
Mathew <mathew@OWC-DT-O-010.(none)>
2013-01-17 17:34:01 +0530

    Malay Translation for UserService Model Validations

--- app/models/user_service.rb
+++ app/models/user_service.rb
@@ -11,19 +11,19 @@ class UserService < ActiveRecord::Base
 
   def first_appointment_to_public_service_should_be_lesser_than_first_appointment_to_the_present_post
     if self.first_appointment_to_public_service.present? && self.first_appointment_to_present_position.present?
-      errors.add(:base,"Date of First Appointment of Public Service Should be Lesser than Date of First Appointment of the Present Post") if self.first_appointment_to_public_service.to_date > self.first_appointment_to_present_position.to_date
+      errors.add(:base,(I18n.translate!('errors_date.app_date_present_post'))) if self.first_appointment_to_public_service.to_date > self.first_appointment_to_present_position.to_date
     end
   end
 
   def first_appointment_to_present_post_should_be_lesser_than_promotion_to_present_grade
     if self.first_appointment_to_present_position.present? && self.promotion_to_present_grade.present?
-      errors.add(:base,"Date of First Appointment of the Present Post Should be Lesser than Date of Promotion to Present Grade") if self.first_appointment_to_present_position.to_date > self.promotion_to_present_grade.to_date
+      errors.add(:base,(I18n.translate!('errors_date.present_post_present_grade'))) if self.first_appointment_to_present_position.to_date > self.promotion_to_present_grade.to_date
     end
   end
 
   def promotion_to_present_grade_should_be_lesser_than_confirmation_date
     if self.promotion_to_present_grade.present? && self.confirmation_date.present?
-      errors.add(:base,"Promotion to Present Grade Should be Lesser than Confirmation Date") if self.promotion_to_present_grade.to_date > self.confirmation_date.to_date
+      errors.add(:base,(I18n.translate!('errors_date.present_grade_confirm_date'))) if self.promotion_to_present_grade.to_date > self.confirmation_date.to_date
     end
   end
 
