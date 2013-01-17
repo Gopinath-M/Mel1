50a2e4b4b1a3af2e1a015666ca69271b1a492381
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-01-17 15:31:59 +0530

    updated model for agency store

--- app/models/agency_store.rb
+++ app/models/agency_store.rb
@@ -10,6 +10,7 @@ class AgencyStore < ActiveRecord::Base
 
   #  validates_uniqueness_of :agency_id, :sub_category_id, :resource_id, :case_sensitive=>false
 #  validates :resource_id, :uniqueness => {:scope => [:resource_id, :sub_category_id]}, :if=>Proc.new{|u| u.resource_type!="Room" || u.resource_type=="Transport" || u.resource_type=="Others" || (u.resource_type=="ICT" && u.serial_no=='')}
+  validates :serial_no, :presence =>true, :if=>Proc.new{|u|  u.resource_type=="ICT" && u.resource.has_serial_no?}
   validates :serial_no, :uniqueness => {:scope => [:agency_id,:resource_id, :sub_category_id, :serial_no]}, :if=>Proc.new{|u| u.resource_type=="ICT" && u.serial_no!='' && u.serial_no!=nil}
   validates :resource_id, :uniqueness => {:scope => [:agency_id,:resource_id, :sub_category_id]}, :if=>Proc.new{|u| u.resource_type=="Room" || u.resource_type=="Others" || (u.resource_type=="ICT" && u.serial_no==nil && u.quantity!=nil)}
   
