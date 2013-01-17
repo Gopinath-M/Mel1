ee7300440e379755a5a881ae76c36733eb62b393
Mathew <mathew@OWC-DT-O-010.(none)>
2013-01-17 13:11:05 +0530

    User Services Changes

--- app/controllers/user_services_controller.rb
+++ app/controllers/user_services_controller.rb
@@ -13,16 +13,25 @@ class UserServicesController < ApplicationController
     if @user_service.valid?
       @user_service.user_id = current_user.id
       @user_service.save
-       redirect_to :controller => 'users', :action => 'emergency_reference'
+      redirect_to :controller => 'users', :action => 'emergency_reference'
     else
       render :action => 'new'
-    end    
+    end
   end
 
   def edit
+    @user_service = UserService.find(params[:id])
   end
 
   def show
   end
 
+  def update
+    @user_service = UserService.find(params[:id]) if params[:id]
+    if @user_service.present? && @user_service.update_attributes(params[:user_service])
+      redirect_to :controller => 'users', :action => 'emergency_reference'
+    else
+      render :action => 'new'
+    end
+  end
 end
