e20941ddacd1aecfd70d49f0385f3027b1bbb288
Mathew <mathew@OWC-DT-O-010.(none)>
2013-01-22 20:43:27 +0530

    Ability File Changes

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -9,12 +9,13 @@ class Ability
     elsif user.is_department_admin?
       can [:read,:create], User
       can :create, Approver
-      can [:read,:create,:show,:approve_request,:request_view,:get_vehicles,:get_driver_details,:change_resource_status], ResourceTransportationBooking
-      can [:read,:create,:show,:list_resource_booking,:room_booking_approval],ResourceRoomBooking
-      can [:read,:create,:show,:approve_request,:requests],ResourceIctEquipmentBooking
-      can [:read,:create,:show,:list_resource_booking,:resource_booking_approval],ResourceBooking
-      can [:read,:update,:list_ict_firewall],IctFirewall
-      can [:read,:update,:approve_request,:approve_request_for_state],Outstation
+      #can [:read,:create,:show,:approve_request,:request_view,:get_vehicles,:get_driver_details,:change_resource_status], ResourceTransportationBooking
+      #can [:read,:create,:show,:list_resource_booking,:room_booking_approval],ResourceRoomBooking
+      #can [:read,:create,:show,:approve_request,:requests],ResourceIctEquipmentBooking
+      #can [:read,:create,:show,:list_resource_booking,:resource_booking_approval],ResourceBooking
+      #can [:read,:update,:list_ict_firewall],IctFirewall
+      #can [:read,:update,:approve_request,:approve_request_for_state],Outstation
+      can [:manage], [ResourceTransportationBooking,ResourceRoomBooking,ResourceIctEquipmentBooking,ResourceBooking,IctFirewall,Outstation]
 
     elsif user.is_department_user?
 
@@ -34,11 +35,12 @@ class Ability
 
     elsif user.is_resource_manager?
 
-      can [:read,:create,:show,:approve_request,:request_view,:get_vehicles,:get_driver_details,:change_resource_status], ResourceTransportationBooking
-
+      #can [:read,:create,:show,:approve_request,:request_view,:get_vehicles,:get_driver_details,:change_resource_status], ResourceTransportationBooking
+      can [:manage], [ResourceTransportationBooking,ResourceRoomBooking,ResourceIctEquipmentBooking,ResourceBooking]
+      
     elsif ( user.is_datuk_suk? || user.is_suk_deputy? || user.is_chief_minister? || user.is_human_resource_manager? )
 
-      can [:read,:update,:approve_request,:approve_request_for_state], Outstation
+      can [:manage], Outstation
 
     end
   end
