07f461adccd4c6681e04beb77571e2b94863e7f3
Mathew <mathew@OWC-DT-O-010.(none)>
2013-01-24 16:19:22 +0530

    Compliants Building Asset & Complaints Computer - Reason - Validation Removed - Sheik Dawood

--- app/models/complaint_computer.rb
+++ app/models/complaint_computer.rb
@@ -1,5 +1,5 @@
 class ComplaintComputer < ActiveRecord::Base
   belongs_to :user
-  validates :complaint_type_id, :system_access_id, :system_model_type_id, :serial_no, :reason, :presence => true
+  validates :complaint_type_id, :system_access_id, :system_model_type_id, :serial_no,  :presence => true
   validates :serial_no, :numericality => { :only_integer => true }
 end
