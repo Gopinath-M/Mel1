b2fad49b22d57e95602e3dd2fbdbd4e57f7d5f10
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-02-01 13:18:50 +0530

    updated migration file for messages

--- db/migrate/20130131204413_add_fields_to_exist_tables.rb
+++ db/migrate/20130131204413_add_fields_to_exist_tables.rb
@@ -1,13 +1,9 @@
 class AddFieldsToExistTables < ActiveRecord::Migration
   def self.up
-    change_column  :groups , :department_id ,:integer
-    change_column  :group_members , :user_id ,:integer
-    add_column  :resource_ict_equipment_bookings , :ict_equipment_attachment ,:string
-    add_column  :ict_hardware_bookings , :ict_hardware_attachment ,:string
+    add_column  :message_comments, :deleted, :boolean, :default => false
   end
 
   def self.down
-    remove_column  :resource_ict_equipment_bookings , :ict_equipment_attachment
-    remove_column  :ict_hardware_bookings , :ict_hardware_attachment
+    remove_column  :message_comments, :deleted
   end
 end
