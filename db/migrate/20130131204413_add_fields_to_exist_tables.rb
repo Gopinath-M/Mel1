e4bd49c98ed51fcaedcf76b0292224e7e9a20615
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-02-01 12:51:22 +0530

    added stand alone migration

---
+++ db/migrate/20130131204413_add_fields_to_exist_tables.rb
@@ -0,0 +1,15 @@
+class AddFieldsToExistTables < ActiveRecord::Migration
+  def self.up
+    change_column  :groups , :department_id ,:integer
+    change_column  :group_members , :user_id ,:integer
+    add_column  :resource_room_bookings, :user_notification_mail ,:boolean, :default =>false
+    add_column  :resource_ict_equipment_bookings , :ict_equipment_attachment ,:string
+    add_column  :ict_hardware_bookings , :ict_hardware_attachment ,:string
+  end
+
+  def self.down
+    remove_column  :resource_room_bookings, :user_notification_mail
+    remove_column  :resource_ict_equipment_bookings , :ict_equipment_attachment
+    remove_column  :ict_hardware_bookings , :ict_hardware_attachment
+  end
+end
