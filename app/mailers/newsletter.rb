b0704bfc4a23f0c32b03304479e423c010ffe7b1
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-02-07 20:56:27 +0530

    updated for newsletter

---
+++ app/mailers/newsletter.rb
@@ -0,0 +1,7 @@
+class Newsletter < ActionMailer::Base
+  default :from => "from@example.com"
+  layout false 
+  def newsletter(user_email, subject, content)
+    mail(:to => user_email, :subject => subject, :content=>content)
+  end
+end
