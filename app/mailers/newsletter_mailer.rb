a034ec5d2c4e27b330cfa148470ab5c4be753ef5
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-02-13 12:39:19 +0530

    updated mailer

--- app/mailers/newsletter_mailer.rb
+++ app/mailers/newsletter_mailer.rb
@@ -1,9 +1,24 @@
 class NewsletterMailer < ActionMailer::Base
   default :from => "from@example.com"
-  layout false 
+  layout false
+  
   def send_newsletter(user_email, subject, content)
     mail(:to => user_email, :subject => subject)do |format|
       format.html { render :text => content }
     end
   end
+
+  def send_template(user_email, subject, content,user)
+    if content.include?("{{user.username}}")
+     content.sub!("{{user.username}}",user.username)
+    end
+    if content.include?("{{user.email}}")
+     content.sub!("{{user.email}}",user.email)
+    end
+
+    mail(:to => user_email, :subject => subject)do |format|
+      format.html { render :text => content }
+    end
+  end
+  
 end
