1c09508de9e5a08e1c918eb720d2b364e3171243
Mathew <mathew@OWC-DT-O-010.(none)>
2013-02-18 22:28:19 +0530

    Fixed the issue in Change dept Admin - Sasi

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -432,11 +432,11 @@ class UsersController < ApplicationController
       val = params[:change_department][:id]
       department = Department.find(params[:change_department][:id])
       admin = User.find_by_ic_number(params[:transfer][:username_admin])
+      role_val = RoleMembership.find_by_user_id_and_role_id(admin.id, 3)
       role = RoleMembership.find_by_department_id_and_role_id(params[:change_department][:id], 2)
-      user = department.users.where("role_id = ?", 2)
-      role_update = RoleMembership.find_by_user_id_and_role_id(admin.id, 3)
-      role.update_attribute(:user_id,admin.id)
-      role_update.update_attribute(:user_id,user.first.id)
+      p role_val.update_attribute(:user_id,role.user_id)
+      user = department.users.where("role_id = ?", 2)      
+      p role.update_attribute(:user_id,admin.id)      
       redirect_to(users_path, :notice => "#{admin.first_name} has been changed as Department Admin to #{department.name} Department.")
     else
       redirect_to(change_department_admin_users_path, :alert => "Please Select the Drop box listed")
