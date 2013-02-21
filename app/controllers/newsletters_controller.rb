a22bf7488f64a4138b0acaf4334d53b006a900d4
nirmala <nirmala@OWC-DT-O-046.(none)>
2013-02-21 11:59:20 +0530

    updated for newsletter

--- app/controllers/newsletters_controller.rb
+++ app/controllers/newsletters_controller.rb
@@ -8,16 +8,16 @@ class NewslettersController < ApplicationController
 
   def create
     if params[:template][:type]=="All Users"
-      newsletter_to = "All Users"
+      temp_newsletter_to = "All Users"
       users = User.active
     elsif params[:template][:type]=="All Dept Admins"
-      newsletter_to = "All Dept Admins"
+      temp_newsletter_to = "All Dept Admins"
       users =  User.active.joins(:role_memberships).where("role_memberships.role_id=2")
     elsif params[:template][:type]=="Select Agency"
       if params[:template][:agency]!=nil
         departments = Department.active.joins(:agency).where(:agency_id=>params[:template][:agency]).collect(&:id)
         users = User.active.joins(:role_memberships).where("role_memberships.department_id in (?)", departments).collect(&:email)
-        newsletter_to = users
+        temp_newsletter_to = users
       else
         flash[:alert] = 'Please select Agency'
       end
@@ -25,14 +25,14 @@ class NewslettersController < ApplicationController
       if params[:template][:agency]!=nil && params[:template][:department_id]!=nil
         department = Department.find_by_agency_id_and_id(params[:template][:agency], params[:template][:department_id])
         users = User.active.joins(:role_memberships).where("role_memberships.department_id = ?", department.id).collect(&:email)
-        newsletter_to = users
-    else
+        temp_newsletter_to = users
+      else
         flash[:alert] = 'Please select Agency and Department'
       end
     elsif  params[:template][:type]=="Select Dept Admin"
       if params[:template][:user_id]!=nil
         users= User.where(:ic_number => params[:template][:user_id]).collect(&:email)
-        newsletter_to = users
+        temp_newsletter_to = users
       else
         flash[:alert] = 'Please select User'
       end
@@ -40,14 +40,23 @@ class NewslettersController < ApplicationController
       flash[:alert] = 'Please select a user type'
     end
     if users && !users.empty?
-      newsletter_to.uniq! if is_array?(newsletter_to)
-      @newsletter = Newsletter.create!(params[:newsletter].merge!({:created_by => current_user.id,:from=>current_user.email, :status => "New",:to=>newsletter_to.join(",").to_s}))
-    if @newsletter.valid?
-      @newsletter.save!
-      redirect_to :action => 'index'
-    else
-      render :action => 'new'
+      if temp_newsletter_to.kind_of?(Array)
+        temp_newsletter_to.uniq!
+        newsletter_to = temp_newsletter_to.join(", ").to_s
+      else
+        newsletter_to = temp_newsletter_to
       end
+     p @newsletter = Newsletter.create!(params[:newsletter].merge!({:created_by => current_user.id,:from=>current_user.email, :status => "New",:to=> newsletter_to}))
+      if @newsletter.valid?
+        @newsletter.save!
+        redirect_to :action => 'index'
+      else
+        @newsletter = Newsletter.new(params[:newsletter])
+        render :action => 'new'
+      end
+    else
+      @newsletter = Newsletter.new(params[:newsletter])
+      render :action => 'new'
     end
   end
 
