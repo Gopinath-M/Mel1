14225bd14c8c498ed0569e7c68a32d6171ebfb18
Nirmala <user@HP-ARJUN.(none)>
2013-02-25 00:02:42 +0530

    checking for migration

--- db/migrate/20130218165709_change_data_type_for_from_date_and_to_date.rb
+++ db/migrate/20130218165709_change_data_type_for_from_date_and_to_date.rb
@@ -1,15 +1,24 @@
 class ChangeDataTypeForFromDateAndToDate < ActiveRecord::Migration
   def self.up
-    change_table :outstations do |t|
-      t.change :from_date, :date
-      t.change :to_date, :date
-    end
+    remove_column :outstations , :from_date
+    remove_column :outstations , :to_date
+    add_column :outstations , :from_date, :date
+    add_column :outstations , :to_date, :date
+
+    #    change_table :outstations do |t|
+    #      t.change :from_date, :date
+    #      t.change :to_date, :date
+    #    end
   end
 
   def self.down
-    change_table :outstations do |t|
-      t.change :from_date, :date
-      t.change :to_date, :date
-    end
+    remove_column :outstations , :from_date
+    remove_column :outstations , :to_date
+    add_column :outstations , :from_date, :date
+    add_column :outstations , :to_date, :date
+    #    change_table :outstations do |t|
+    #      t.change :from_date, :date
+    #      t.change :to_date, :date
+    #    end
   end
 end
