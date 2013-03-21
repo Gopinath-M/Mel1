6a87d76e4716c42b1cf7e4099cf2be61b00ca4d2
Manivannan <manisiva19@gmail.com>
2013-03-21 16:57:46 +0530

    removed standalone migrations from git

--- db/migrate/20130205124455_adding_alloted_boolean_in_resources.rb
+++
@@ -1,9 +0,0 @@
-class AddingAllotedBooleanInResources < ActiveRecord::Migration
-  def self.up
-    add_column  :resources,:alloted,:boolean,:default=>false
-  end
-
-  def self.down
-    remove_column :resources, :alloted
-  end
-end
